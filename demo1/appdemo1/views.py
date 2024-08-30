ea635ec73eec69c8bb9316f2b2db59b1198707bb
BaldwinZaid <bdiazc@usm.cl>
2024-08-30 13:55:34 -0400

    nombres

--- demo1/appdemo1/views.py
+++ demo1/appdemo1/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import alumno
+
+
+def datos(request):
+    
+    todo = alumno.objects.all()
+    #persona = alumno.objects.values_list('name', flat=True)
+    return render(request, 'mostrar.html',{'todo':todo})
 
 # Create your views here.
